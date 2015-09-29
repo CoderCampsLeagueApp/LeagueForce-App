beacc19f89ae6c26352d544492af40b3133a69a0
Hamad Alhassen <halhassen@gmail.com>
2015-09-29 16:40:01 -0700

    animate.css added

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,6 +1,6 @@
 (function() {
 	'use strict';
-	angular.module('app', ['ui.router', 'ui.bootstrap'])
+	angular.module('app', ['ui.router'])
 	.config(Config);
 	Config.$inject = ['$stateProvider', '$urlRouterProvider'];
 	function Config($stateProvider, $urlRouterProvider) {
