aaa69ca2f99a780f814f81b1bbcfa26982659474
sinhvis <sinhvis@gmail.com>
2015-10-07 11:40:37 -0700

    Google authentication changed

--- config/passport.js
+++ config/passport.js
@@ -35,13 +35,6 @@ passport.use(new LocalStrategy(function(username, password, done) {
 	});
 }));
 
-// function generateFacebookPhotoUrl(id, accessToken) {
-// 	var picUrl = "https://graph.facebook.com/" ;
-// 	picUrl = picUrl + id ;
-// 	picUrl = picUrl + "/picture?width=300&height=300&access_token=" ;
-// 	picUrl = picUrl + accessToken ;
-// 	return picUrl ;
-// }
 
 function generateFacebookPhotoUrl(id, accessToken, height, width) {
 	var picUrl = "https://graph.facebook.com/" ;
@@ -101,9 +94,9 @@ function(accessToken, refreshToken, profile, done) {
 
 				// Photo
 				newUser.facebook.photo = profile.photos[0].value ;
-				// console.log("DEBUG: passport.js profile photo: " + newUser.facebook.photo) ;
-
-				// newUser.pic = newUser.facebook.photo ;
+				
+				// Getting bigger photo URL from Facebook
+				// Sending size 300x300.
 				newUser.pic = generateFacebookPhotoUrl(profile.id, accessToken, 300, 300) ;
 
 				// Created. Stores date created in the database.
@@ -150,8 +143,8 @@ function(accessToken, refreshToken, profile, done) {
 		// Whatever is returned will be stored in profile.
 		// Returns err if it cannot connect
 		User.findOne({ 'google.id' : profile.id }, function(err, user) {
-			console.log("DEBUG: Contents of profile:") ;
-			console.log(profile) ;
+			// console.log("DEBUG: Contents of profile:") ;
+			// console.log(profile) ;
 			if(err) {
 				console.log('DEBUG: Error connecting') ;
 				return done(err) ;
@@ -178,16 +171,16 @@ function(accessToken, refreshToken, profile, done) {
 
 				// According to Google API, emails come back as an array
 				// So, need the first element of the array.
-				newUser.google.email = profile.emails ? profile.emails[0].value : profile.username + "@facebook.com";
+				newUser.google.email = profile.emails ? profile.emails[0].value : profile.username + "@google.com";
 				// Setting username to email from Facebook
 				newUser.username = newUser.google.email ;
 
 				// Photo
 				newUser.google.photo = profile.photos[0].value ;
 
-				// Get bigger photo URL from Google.
-				newUser.google.photo = generateGooglePhotoUrl(newUser.google.photo, 300) ;
-				newUser.pic = newUser.google.photo;
+				// Get bigger photo URL from Google. Sending size = 300
+				newUser.pic = generateGooglePhotoUrl(newUser.google.photo, 300) ;
+				
 
 				// Created stores date created in the database.
 				newUser.created = new Date() ;
@@ -204,50 +197,3 @@ function(accessToken, refreshToken, profile, done) {
 }) ;
 }
 )) ;
-
-
-
-
-// Following is from Isaiah's example	
-// passport.serializeUser(function(user, done) {
-// 	done(null, user) ;
-// }) ;
-
-// passport.deserializeUser(function(obj, done) {
-// 	done(null, obj) ;
-// }) ;
-
-// passport.use(new FacebookStrategy({
-// 	clientID: "DEBUG: Needs to be filled out",
-// 	clientSecret: "DEBUG: Needs to be filled out",
-// 	callbackURL: "DEBUG: This URL needs to be filled out",
-// 	passReqToCallback: true,
-// 	profileFields: ['id', 'name', 'emails', 'photos']
-// },
-// function(req, accessToken, refreshToken, profile, done) {
-// 	User.findOne({ facebookId : profile.id }, function(err, user) {
-// 		if(err) return done(err, null) ;
-// 		if(user) {
-// 			console.log("Current User, Logging In") ;
-// 			return done(null, user) ;
-// 		} else {
-// 			console.log("New User, Registering and Logging In") ;
-// 			var userModel = new User() ;
-// 			if(profile.emails) {
-// 				userModel.username = profile.emails[0].value ;
-// 			} else {
-// 				userModel.username = profile.username + "@facebook.com" ;
-// 			}
-// 			userModel.facebookId = profile.id ;
-// 			userModel.name = profile.name.givenName + " " + profile.name.familyName ;
-// 			userModel.save(function(err, userSaved) {
-// 				if(err) {
-// 					return err ;
-// 				}
-// 				return(err, userSaved) ;
-// 			})
-// 		}
-// 	}) ;
-// }
-// )) ;
-
