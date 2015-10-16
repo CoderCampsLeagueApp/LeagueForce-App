3fbe32ab514ebaabd1149a0e4ba1fc8eea00efa2
sinhvis <sinhvis@gmail.com>
2015-10-16 11:24:18 -0700

    auth parameters changed for Google

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,30 @@
+// The following is for local login only.
+
+// module.exports = {
+// 	'facebookAuth' : {
+// 		'clientID': '1505739546402847',
+// 		'clientSecret': '2bb564141585751b8b802c88e79e1894',
+// 		'callbackURL': 'http://localhost:3000/api/user/auth/facebook/callback'
+// 	},
+
+// 	'googleAuth' : {
+// 		'clientID': '292506933241-cblnt3jliavfenqn43mlqiop0nsa1s70.apps.googleusercontent.com',
+// 		'clientSecret': 'xBlb0_dkARcoksHR_7z69CJC',
+// 		'callbackURL': 'http://localhost:3000/api/user/auth/google/callback'
+// 	}
+// }
+
+// For Heroku deployment.
 module.exports = {
 	'facebookAuth' : {
 		'clientID': '1505739546402847',
 		'clientSecret': '2bb564141585751b8b802c88e79e1894',
-		'callbackURL': 'http://localhost:3000/api/user/auth/facebook/callback'
+		'callbackURL': 'https://salty-wave-3423.herokuapp.com/api/user/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
 		'clientID': '292506933241-cblnt3jliavfenqn43mlqiop0nsa1s70.apps.googleusercontent.com',
 		'clientSecret': 'xBlb0_dkARcoksHR_7z69CJC',
-		'callbackURL': 'http://localhost:3000/api/user/auth/google/callback'
+		'callbackURL': 'https://salty-wave-3423.herokuapp.com/api/user/auth/google/callback'
 	}
 }
