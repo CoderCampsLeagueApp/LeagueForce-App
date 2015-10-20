e754c3ac4c710ffc2d8b454cbcafd97c75cec7f8
Hamad Alhassen <halhassen@gmail.com>
2015-10-20 15:20:56 -0700

    adjusting auths for live

--- config/auth.js
+++ config/auth.js
@@ -15,31 +15,31 @@
 // }
 
 // For Heroku deployment.
-module.exports = {
-	'facebookAuth' : {
-		'clientID': '1505739546402847',
-		'clientSecret': '2bb564141585751b8b802c88e79e1894',
-		'callbackURL': 'https://salty-wave-3423.herokuapp.com/api/user/auth/facebook/callback'
-	},
-
-	'googleAuth' : {
-		'clientID': '292506933241-cblnt3jliavfenqn43mlqiop0nsa1s70.apps.googleusercontent.com',
-		'clientSecret': 'xBlb0_dkARcoksHR_7z69CJC',
-		'callbackURL': 'https://salty-wave-3423.herokuapp.com/api/user/auth/google/callback'
-	}
-}
-
-//leagueforce.us
 // module.exports = {
 // 	'facebookAuth' : {
 // 		'clientID': '1505739546402847',
 // 		'clientSecret': '2bb564141585751b8b802c88e79e1894',
-// 		'callbackURL': 'https://leagueforce.us/api/user/auth/facebook/callback'
+// 		'callbackURL': 'https://salty-wave-3423.herokuapp.com/api/user/auth/facebook/callback'
 // 	},
 
 // 	'googleAuth' : {
 // 		'clientID': '292506933241-cblnt3jliavfenqn43mlqiop0nsa1s70.apps.googleusercontent.com',
 // 		'clientSecret': 'xBlb0_dkARcoksHR_7z69CJC',
-// 		'callbackURL': 'https://leagueforce.us/api/user/auth/google/callback'
+// 		'callbackURL': 'https://salty-wave-3423.herokuapp.com/api/user/auth/google/callback'
 // 	}
 // }
+
+//leagueforce.us
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1505739546402847',
+		'clientSecret': '2bb564141585751b8b802c88e79e1894',
+		'callbackURL': 'https://leagueforce.us/api/user/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': '292506933241-cblnt3jliavfenqn43mlqiop0nsa1s70.apps.googleusercontent.com',
+		'clientSecret': 'xBlb0_dkARcoksHR_7z69CJC',
+		'callbackURL': 'https://leagueforce.us/api/user/auth/google/callback'
+	}
+}
